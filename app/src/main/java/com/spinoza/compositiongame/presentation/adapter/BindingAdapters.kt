3b0dfc479661efbba534e091a98b3e41b407d581
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-01-25 19:54:46 +0300

    add data binding to GameFragment

--- app/src/main/java/com/spinoza/compositiongame/presentation/adapter/BindingAdapters.kt
+++ app/src/main/java/com/spinoza/compositiongame/presentation/adapter/BindingAdapters.kt
@@ -1,40 +1,85 @@
 package com.spinoza.compositiongame.presentation.adapter
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.spinoza.compositiongame.R
 import com.spinoza.compositiongame.domain.entity.GameResult
 import com.spinoza.compositiongame.presentation.calculatePercent
 
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
+
 @BindingAdapter("requiredAnswers")
-fun findRequiredAnswers(textView: TextView, requiredScore: Int) {
+fun bindRequiredAnswers(textView: TextView, requiredScore: Int) {
     textView.text =
         String.format(textView.context.getString(R.string.required_score), requiredScore)
 }
 
 @BindingAdapter("scoreAnswers")
-fun findScoreAnswers(textView: TextView, scoreAnswers: Int) {
+fun bindScoreAnswers(textView: TextView, scoreAnswers: Int) {
     textView.text = String.format(textView.context.getString(R.string.score_answers), scoreAnswers)
 }
 
 @BindingAdapter("requiredPercentage")
-fun findRequiredPercentage(textView: TextView, requiredPercentage: Int) {
+fun bindRequiredPercentage(textView: TextView, requiredPercentage: Int) {
     textView.text =
         String.format(textView.context.getString(R.string.required_percentage), requiredPercentage)
 }
 
 @BindingAdapter("scorePercentage")
-fun findScorePercentage(textView: TextView, gameResult: GameResult) {
+fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
     textView.text = String.format(textView.context.getString(R.string.score_percentage),
         calculatePercent(gameResult.countOfRightAnswers, gameResult.countOfQuestions))
 }
 
 @BindingAdapter("emojiResult")
-fun findEmojiResult(imageView: ImageView, winner: Boolean) {
+fun bindEmojiResult(imageView: ImageView, winner: Boolean) {
     imageView.setImageResource(if (winner) {
         R.drawable.ic_smile
     } else {
         R.drawable.ic_sad
     })
+}
+
+@BindingAdapter("percentOfRightAnswers")
+fun bindPercentOfRightAnswers(progressBar: ProgressBar, percent: Int) {
+    progressBar.setProgress(percent, true)
+}
+
+@BindingAdapter("enoughPercentOfRightAnswers")
+fun bindEnoughPercentOfRightAnswers(progressBar: ProgressBar, goodState: Boolean) {
+    progressBar.progressTintList =
+        ColorStateList.valueOf(getColorByState(progressBar.context, goodState))
+}
+
+@BindingAdapter("enoughCountOfRightAnswers")
+fun bindEnoughCountOfRightAnswers(textView: TextView, goodState: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, goodState))
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
 }
